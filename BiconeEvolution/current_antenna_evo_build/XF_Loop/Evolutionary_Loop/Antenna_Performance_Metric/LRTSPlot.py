49e802e0d922c0ea07ac1858bfc18228340b08ae
machtay1 <machtay1@pitzer-rw02.ten.osc.edu>
2020-09-01 15:16:07 -0400

    Minor plotting improvements.

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/LRTSPlot.py
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/LRTSPlot.py
@@ -9,7 +9,20 @@ import numpy as np		# for data manipulation, storage
 import matplotlib.pyplot as plt	# For plotting
 import os			# exclusively for rstrip()
 import argparse			# for getting the user's arguments from terminal
+import matplotlib.cm as cm # for getting colors for plotting
+
 # May be needed: from mpl_toolkits.mplot3d import Axes3D 
+
+## Update (Machtay 7/23/20): I'm just including a comment on how to run this script
+## To run, pass the following arguments:
+### 1. Working Directory (ex: /path/to/Evolutionary_Loop)
+### 2. RunName directory (ex: /path/to/Machtay_NoDatabase_20200721)
+### 3. The next generation number (ex: 3 if you're on generation 2)
+### 4. NPOP (ex: 10)
+### 5. Geometric Scale Factor (which is now defunct) (ex: 1)
+### 6. Number of sections (usually 2 if using this)
+## For example, run as follows (from the working directory):
+## python Antenna_Performance_Metric/LRTSPlot.py . ./Run_Outputs/Machtay_20200831_Asym_Length_and_Angle 11 10 1 2
 
 #---------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES----------GLOBAL VARIABLES
 
@@ -35,7 +48,7 @@ for i in range(g.NPOP):
 	gen_num_1 = []
 	gen_num_2 = []
 	for j in range(g.numGens):
-		k = np.random.uniform(-1/10, 1/10)
+		k = np.random.uniform(1/10, 3/10)
 		gen_num_1.append(j + k)
 		gen_num_2.append(j - k)
 	gen_array_1.append(gen_num_1)
@@ -178,16 +191,41 @@ axT = fig.add_subplot(1,3,3)
 #axO = fig.add_subplot(1,4,4)
 
 # Loop through each individual and plot each array
+# I want to have a more generic color scheme so that I can use more than 20 individuals
 color={1:'red',2:'olive',3:'mediumturquoise',4:'blue',5:'gold',6:'darkred',7:'green',8:'lime',9:'orange',10:'indigo',11:'dimgrey',12:'rosybrown',13:'lightcoral',14:'firebrick',15:'maroon',16:'sienna',17:'sandybrown',18:'peachpuff',19:'peru',20:'tan'}
+colors = cm.rainbow(np.linspace(0, 1, g.NPOP))
 for ind in range(g.NPOP):
 	LabelName = "Individual {}".format(ind+1)
-	E = np.random.uniform(-1/3, 1/3)
-	axL.plot(gen_array_1[ind], length1Array[ind], color=color.get(ind+1, 'black'), marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
-	axL.plot(gen_array_2[ind], length2Array[ind], color=color.get(ind+1, 'black'), marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
-	axR.plot(gen_array_1[ind], radii1Array[ind], color=color.get(ind+1, 'black'), marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
-	axR.plot(gen_array_2[ind], radii2Array[ind], color=color.get(ind+1, 'black'), marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
-	axT.plot(gen_array_1[ind], theta1Array[ind], color=color.get(ind+1, 'black'), marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
-	axT.plot(gen_array_2[ind], theta2Array[ind], color=color.get(ind+1, 'black'), marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+	# plotting the length
+
+	# top
+	axL.plot(gen_array_1[ind], length1Array[ind], color = colors[ind], marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+	
+	# bottom
+	axL.plot(gen_array_2[ind], length2Array[ind], color = colors[ind] , marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+
+	#################################################################################
+
+	# plotting the inner radius
+	# top
+	axR.plot(gen_array_1[ind], radii1Array[ind], color = colors[ind], marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+
+	# bottom
+	axR.plot(gen_array_2[ind], radii2Array[ind], color = colors[ind], marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+
+	##################################################################################
+
+	# plotting the opening angle
+	# top
+	axT.plot(gen_array_1[ind], theta1Array[ind], color = colors[ind], marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+	
+	# bottom
+	axT.plot(gen_array_2[ind], theta2Array[ind], color = colors[ind], marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=10)
+
+	###################################################################################
+
+	# in the future we may want to plot the opening angle too
+
 	#axS.plot(sepArray[ind], color=color.get(ind+1, 'black'), marker = 'o', label = LabelName, linestyle = '')
 	#axO.plot(bigRadii[ind], marker = 'o', label = LabelName, linestyle = '')
 
@@ -209,6 +247,31 @@ axR.set_title("Radius over Generations (0 - {})".format(int(g.numGens-1)), size 
 axT.set_xlabel("Generation", size = 18)
 axT.set_ylabel("Theta [Degrees]", size = 18)
 axT.set_title("Theta over Generations (0 - {})".format(int(g.numGens-1)), size = 20)
+
+# I wanna set the tick marks
+
+axL.set_xticks(np.arange(0, g.numGens, 1))
+axR.set_xticks(np.arange(0, g.numGens, 1))
+axT.set_xticks(np.arange(0, g.numGens, 1))
+
+# Let's make the plots gridded
+
+# length grid
+axL.grid()
+axL.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axL.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
+# radius grid
+axR.grid()
+axR.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axR.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
+# theta grid
+
+axT.grid()
+axT.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axT.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
 '''
 #Separation subplot
 axS.set_xlabel("Generation", size = 18)
@@ -241,14 +304,99 @@ plt.savefig(g.destination + "/" + PlotName)
 plt.show(block=False)
 plt.pause(5)
 
+# Here's a plot of the outer radius
+
 fig = plt.figure(figsize = (10, 8))
+axO = fig.add_subplot(1, 1, 1)
 for i in range(g.NPOP):
     LabelName = "Individual {}".format(ind+1)
-    plt.plot(gen_array_1[ind], bigRadii1[i], color=color.get(i+1, 'black'), marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize = 18)
-    plt.plot(gen_array_2[ind], bigRadii2[i], color=color.get(i+1, 'black'), marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize = 18)
-plt.xlabel('Generation')
-plt.ylabel('Outer Radius [cm]')
-plt.title('Outer Radius vs. Generation')
+    axO.plot(gen_array_1[ind], bigRadii1[i], color=color.get(i+1, 'black'), marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize = 13)
+    axO.plot(gen_array_2[ind], bigRadii2[i], color=color.get(i+1, 'black'), marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize = 13)
+
+# set the labels
+axO.set_xlabel('Generation')
+axO.set_ylabel('Outer Radius [cm]')
+axO.set_title('Outer Radius vs. Generation')
+
+# make it gridded
+axO.grid()
+axO.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axO.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
 plt.savefig(g.destination + "/" + "Outer_Radii")
 plt.show(block=False)
 plt.pause(5)
+
+
+# I think it'll be worthwhile to grab the LRTp lots separately too
+# We don't need to show them, but it's worth saving them
+
+# make a plot of the lengths
+fig_L = plt.figure(figsize = (10, 8))
+axL2 = fig_L.add_subplot(1, 1, 1)
+
+for ind in range(g.NPOP):
+	LabelName = "Individual {}".format(ind+1)
+	
+	# top
+	axL2.plot(gen_array_1[ind], length1Array[ind], color = colors[ind], marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=13)
+	# bottom
+	axL2.plot(gen_array_2[ind], length2Array[ind], color = colors[ind] , marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=13)
+
+axL2.set_xticks(np.arange(0, g.numGens, 1))
+axL2.grid()
+axL2.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axL2.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
+axL2.set_xlabel('Generation')
+axL2.set_ylabel('Length [cm]')
+axL2.set_title('Length vs. Generation')
+
+plt.savefig(g.destination + "/" + "Lengths")
+
+# make a plot of the inner radius
+fig_R = plt.figure(figsize = (10, 8))
+axR2 = fig_R.add_subplot(1, 1, 1)
+
+for ind in range(g.NPOP):
+	LabelName = "Individual {}".format(ind+1)
+	# top
+	axR2.plot(gen_array_1[ind], radii1Array[ind], color = colors[ind], marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=13)
+	# bottom
+	axR2.plot(gen_array_2[ind], radii2Array[ind], color = colors[ind], marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=13)
+
+axR2.set_xticks(np.arange(0, g.numGens, 1))
+axR2.grid()
+axR2.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axR2.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
+axR2.set_xlabel('Generation')
+axR2.set_ylabel('Radius [cm]')
+axR2.set_title('Inner Radius vs. Generation')
+
+plt.savefig(g.destination + "/" + "Inner_Radii")
+
+# make a plot of the opening angle
+fig_T = plt.figure(figsize = (10, 8))
+axT2 = fig_T.add_subplot(1, 1, 1)
+
+for ind in range(g.NPOP):
+	LabelName = "Individual {}".format(ind+1)
+	# top
+	axT2.plot(gen_array_1[ind], theta1Array[ind], color = colors[ind], marker = 'o', label = LabelName, linestyle = '', alpha = 0.4, markersize=13)
+	
+	# bottom
+	axT2.plot(gen_array_2[ind], theta2Array[ind], color = colors[ind], marker = 'x', label = LabelName, linestyle = '', alpha = 0.4, markersize=13)
+
+axT2.set_xticks(np.arange(0, g.numGens, 1))
+axT2.grid()
+axT2.xaxis.grid(linestyle = '--', linewidth = 0.5)
+axT2.yaxis.grid(linestyle = '--', linewidth = 0.5)
+
+axT2.set_xlabel('Generation')
+axT2.set_ylabel('Angle [cm]')
+axT2.set_title('Opening Angle vs. Generation')
+
+plt.savefig(g.destination + "/" + "Opening_Angle")
+
+
